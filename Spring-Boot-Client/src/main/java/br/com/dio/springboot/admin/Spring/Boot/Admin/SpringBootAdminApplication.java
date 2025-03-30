7ded04cede4efbe50a469bfbed634027e466648e
Robson <nosboro@gmail.com>
2025-03-30 15:08:43 -0300

    Add Spring-Boot-Client

---
+++ Spring-Boot-Client/src/main/java/br/com/dio/springboot/admin/Spring/Boot/Admin/SpringBootAdminApplication.java
@@ -0,0 +1,16 @@
+package br.com.dio.springboot.admin.Spring.Boot.Admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class SpringBootAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-Client/src/test/java/br/com/dio/springboot/admin/Spring/Boot/Admin/SpringBootAdminApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.dio.springboot.admin.Spring.Boot.Admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+
+
+@SpringBootApplication
+class SpringBootAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
